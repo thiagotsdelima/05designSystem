0427f077bebf1b886232c182bc8e5818ffa19e3d
thiago <thiagotsdelima@outlook.com>
2024-05-20 16:42:28 -0300

    create stitches
    create stitches

---
+++ packages/react/src/index.tsx
@@ -0,0 +1,11 @@
+import { styled } from './styles'
+
+const Button = styled('button', {
+  fontFamily: '$default',
+  background: '$ignite500',
+  borderRadius: '$md',
+})
+
+export function App() {
+  return <Button>hello world</Button>  
+}
